ff7843a84a9b5a0d0a78ceffb0fab1cdb90c0271
Seongho Jang <jangsung0212@naver.com>
2022-11-07 23:41:22 +0900

    11.07

---
+++ real.R
@@ -0,0 +1,83 @@
+detach(package:ROCR)
+detach(package:neuralnet)
+library(dplyr)
+
+df = read.csv("C:/Users/User/Desktop/Social_Network_Ads.csv")# csv파일 read
+
+df
+
+summary(df)
+plot(df)
+plot(df$EstimatedSalary,df$Purchased)
+plot(df$Age,df$Purchased)
+
+table(df$Purchased)
+
+df[df$Gender=='Male',"Gender"] = 1
+df[df$Gender=='Female',"Gender"] = 0
+df = df[2:5]
+df
+df$Gender = as.numeric(df$Gender)
+
+z = df[1:4]
+z = as.data.frame(z)
+typeof(z)
+z
+
+beta0 = sample(0:1)
+beta1 = sample(0:1,1)
+beta2 = sample(0:1,1)
+beta3 = sample(0:1,1)
+w = c(beta1,beta2,beta3)
+w
+z = z %>% mutate(cal = (beta0+beta1*Gender+ beta2*Age + beta3*EstimatedSalary))
+z = as.data.frame(z)
+
+
+sigmoid = function(z){
+  return(1/(1+exp(-z)))
+}
+
+q = matrix(0, nrow(df), 1)
+q # return y값
+
+cost = function(z){
+  for (i in 1:nrow(z)) {
+    t = z[i,5]
+    if (z[i,4] == 1)  {
+      q[i] == -log(sigmoid(t))
+      # return(q[i])
+    }else{
+      q[i] == -(1-log(sigmoid(t)))
+      # return(q[i])
+    }
+  }
+}
+
+cost(z)
+
+g_desc = function(fp,x,h=1e-2, tol = 1e-4, m=1e3){
+  iter = 0
+  oldx = x
+  x = x -h*fp(x) # initial gradient만큼 이동
+  while (abs(x-oldx)>tol){
+    iter = iter +1
+    if (iter > m) stop("max iteration")
+    oldx = x
+    x = x -h*fp(x) # 부호가 +라면 gradient ascent
+  }
+  return (x)
+}
+
+z = as.matrix(z)
+z= z[1:3]
+w
+
+loss_fun = function(x, t){
+  delta = 1e-7
+  k = z%*%w + beta0
+  y = sigmoid(z)
+  return(sum(t*log(y+delta)+(1-t)*log(1-y)+delta))
+}
+
+loss_fun(x,1)
--- task_logistic2.R
+++ task_logistic2.R
@@ -9,11 +9,13 @@ head(df)
 summary(df)
 
 #Gender값 1,0 변경
+df
 df[df$Gender=='Male',"Gender"] = 1
 df[df$Gender=='Female',"Gender"] = 0
 df = df[2:5]
 df
-df$Gender
+df$Gender = as.numeric(df$Gender)
+typeof(df$Gender)
 
 #변수간의 관계 plot
 plot(df)
@@ -46,43 +48,44 @@ sigmoid = function(z){
 loss_fun = function(y,a){
   return(J = -(y*log(a)+(1-y)*log(1-a)))
 }
-
-train = 0.7*nrow(df)
-train
-train_ = df[1:tarin,]
-test_ = df[tarin+1:120,]
-train_
-test_
-colnames(df)
-
-model = glm(Purchased ~ ., data = train_,family = 'binomial')
-model
-summary(model)
-
-pred = predict(model, newdata = test_, type = "response")
-head(pred)
-head(test_$Purchased)
-
-result_pred = ifelse(pred>=0.5,1,0)
-Table = table(result_pred,test_$Purchased)
-Table # confusion matrix
-
-total = matrix(0,nrow(test_),1)
-
-# library(ROCR)
-# pr = prediction(pred,test_$Purchased)
-# prf = performance(pr,measure = "tpr", x.measure = "fpr")
-# plot(prf,main = "ROC Curve")
-
-for (i in nrow(test_)) {
-  k = pred[i]
-  total[i] = sigmoid(i)
-  return (total[i])
-}
-
-sigmoid
-pred
-plot(pred)
+# 
+# #tarin set, test set
+# train = 0.7*nrow(df)
+# train
+# train_ = df[1:tarin,]
+# test_ = df[tarin+1:120,]
+# train_
+# test_
+# colnames(df)
+# 
+# model = glm(Purchased ~ ., data = train_,family = 'binomial')
+# model
+# summary(model)
+# 
+# pred = predict(model, newdata = test_, type = "response")
+# head(pred)
+# head(test_$Purchased)
+# 
+# result_pred = ifelse(pred>=0.5,1,0)
+# Table = table(result_pred,test_$Purchased)
+# Table # confusion matrix
+# 
+# total = matrix(0,nrow(test_),1)
+# 
+# # library(ROCR)
+# # pr = prediction(pred,test_$Purchased)
+# # prf = performance(pr,measure = "tpr", x.measure = "fpr")
+# # plot(prf,main = "ROC Curve")
+# 
+# for (i in nrow(test_)) {
+#   k = pred[i]
+#   total[i] = sigmoid(i)
+#   return (total[i])
+# }
+# 
+# sigmoid
+# pred
+# plot(pred)
 
 print("-------------------------------------------")
 
@@ -90,7 +93,6 @@ colnames(df)
 
 ggplot(df,aes(x=Age,y=Purchased)) + geom_point()
 ggplot(df,aes(x=Age,y=Purchased)) + geom_point() + stat_smooth()
-ggplot(df,aes(x=Age,y=Purchased)) + geom_point() + stat_smooth('lm')
 
 
 ggplot(df,aes(x=EstimatedSalary,y=Purchased)) + geom_point()
@@ -104,6 +106,15 @@ z$coefficients
 
 beta0 = c(z$coefficients[1],z$coefficients[2],z$coefficients[3],z$coefficients[4])
 
+beta0 = as.matrix(beta0)
+beta0[1]
+beta0[2]
+beta0[3]
+beta0[4]
+typeof(beta0)
+
+typeof(df[1,1])
+
 nrow(test_)
 
 
@@ -114,25 +125,30 @@ q
 beta0[4]*test_[1,4]
 q[1] = beta0[1] * test_[1,1]
 beta0[1]
-test_ = as.data.frame(test_)
-test_$Gender=as.numeric(test_$Gender)
-test_$Age=as.numeric(test_$Age)
-test_$EstimatedSalary=as.numeric(test_$EstimatedSalary)
-test_$Purchased=as.numeric(test_$Purchased)
+# test_ = as.data.frame(test_)
+# test_$Gender=as.numeric(test_$Gender)
+# test_$Age=as.numeric(test_$Age)
+# test_$EstimatedSalary=as.numeric(test_$EstimatedSalary)
+# test_$Purchased=as.numeric(test_$Purchased)
 
 
 typeof(test_$Gender)
 typeof(beta0[1])
 
 i = 1
-for (i in 1:nrow(test_)) {
-  for (k in 1:ncol(test_)) {
-    w[i,k] = beta0[k] * test_[i,k]
+k = 1
+for (i in 1:nrow(df)+1) {
+  for (k in 1:ncol(df)+1) {
+    w[i,k] = beta0[k] * df[i,k]
     return(w)
   }
   q[i] = w[i,1] + w[i,2] + w[i,3] + beta0[1]
   return(q)
 }
+
+w[1,2]=beta0[1]*df[1,2]
+
+df[1,2]
 
 w
 q
